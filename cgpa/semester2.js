61c1953bebc2cab7ef17b3df58cd711ffc39953e
Akib Reza <akib.reza23@gmail.com>
2023-09-29 10:46:21 +0600

    v3.1 back button + cgpa complete for 3 semester

---
+++ cgpa/semester2.js
@@ -0,0 +1,70 @@
+document.addEventListener("DOMContentLoaded", function () {
+  // Get references to the input fields
+  const cse151 = document.getElementById("cse151");
+  const cse152 = document.getElementById("cse152");
+  const cse153 = document.getElementById("cse153");
+  const eee251 = document.getElementById("eee251");
+  const eee252 = document.getElementById("eee252");
+  const mat165 = document.getElementById("mat165");
+  const me102 = document.getElementById("me102");
+  const ged117 = document.getElementById("ged117");
+  const cse104 = document.getElementById("cse104");
+
+  // Define an object to store the values
+  const subjectValues = {};
+
+  // Add event listeners to input fields to update the object
+  cse151.addEventListener("input", function () {
+    subjectValues.cse151 = parseFloat(this.value) * 3;
+  });
+
+  cse152.addEventListener("input", function () {
+    subjectValues.cse152 = parseFloat(this.value) * 1.5;
+  });
+
+  cse153.addEventListener("input", function () {
+    subjectValues.cse153 = parseFloat(this.value) * 3;
+  });
+
+  eee251.addEventListener("input", function () {
+    subjectValues.eee251 = parseFloat(this.value) * 3;
+  });
+
+  eee252.addEventListener("input", function () {
+    subjectValues.eee252 = parseFloat(this.value) * 1;
+  });
+
+  mat165.addEventListener("input", function () {
+    subjectValues.mat165 = parseFloat(this.value) * 3;
+  });
+
+  me102.addEventListener("input", function () {
+    subjectValues.me102 = parseFloat(this.value) * 1;
+  });
+
+  ged117.addEventListener("input", function () {
+    subjectValues.ged117 = parseFloat(this.value) * 2;
+  });
+  cse104.addEventListener("input", function () {
+    subjectValues.cse104 = parseFloat(this.value) * 1;
+  });
+
+  // Example: Display the object in console when a button is clicked
+
+  const submitButton = document.getElementById("submitButton");
+
+  submitButton.addEventListener("click", function (event) {
+    event.preventDefault();
+
+    if (Object.keys(subjectValues).length === 9) {
+      let finalResult = 0;
+
+      for (const key in subjectValues) {
+        finalResult += subjectValues[key];
+      }
+
+      finalResult = finalResult / 18.5;
+      console.log(finalResult.toFixed(2));
+    }
+  });
+});
---
+++ cgpa/semester3.js
@@ -0,0 +1,70 @@
+document.addEventListener("DOMContentLoaded", function () {
+  // Get references to the input fields
+  const cse211 = document.getElementById("cse211");
+  const cse212 = document.getElementById("cse212");
+  const cse256 = document.getElementById("cse256");
+  const cse215 = document.getElementById("cse215");
+  const cse216 = document.getElementById("cse216");
+  const chem176 = document.getElementById("chem176");
+  const mat261 = document.getElementById("mat261");
+  const ged119 = document.getElementById("ged119");
+  const chem175 = document.getElementById("chem175");
+
+  // Define an object to store the values
+  const subjectValues = {};
+
+  // Add event listeners to input fields to update the object
+  cse211.addEventListener("input", function () {
+    subjectValues.cse211 = parseFloat(this.value) * 3;
+  });
+
+  cse212.addEventListener("input", function () {
+    subjectValues.cse212 = parseFloat(this.value) * 1.5;
+  });
+
+  cse256.addEventListener("input", function () {
+    subjectValues.cse256 = parseFloat(this.value) * 1.5;
+  });
+
+  cse215.addEventListener("input", function () {
+    subjectValues.cse215 = parseFloat(this.value) * 3;
+  });
+
+  cse216.addEventListener("input", function () {
+    subjectValues.cse216 = parseFloat(this.value) * 1;
+  });
+
+  chem176.addEventListener("input", function () {
+    subjectValues.chem176 = parseFloat(this.value) * 0.8;
+  });
+
+  mat261.addEventListener("input", function () {
+    subjectValues.mat261 = parseFloat(this.value) * 3;
+  });
+
+  ged119.addEventListener("input", function () {
+    subjectValues.ged119 = parseFloat(this.value) * 2;
+  });
+  chem175.addEventListener("input", function () {
+    subjectValues.chem175 = parseFloat(this.value) * 3;
+  });
+
+  // Example: Display the object in console when a button is clicked
+
+  const submitButton = document.getElementById("submitButton");
+
+  submitButton.addEventListener("click", function (event) {
+    event.preventDefault();
+
+    if (Object.keys(subjectValues).length === 9) {
+      let finalResult = 0;
+
+      for (const key in subjectValues) {
+        finalResult += subjectValues[key];
+      }
+
+      finalResult = finalResult / 18.8;
+      console.log(finalResult.toFixed(2));
+    }
+  });
+});
