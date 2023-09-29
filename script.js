260c2cbd9bf14703385f176193a9f5d750d826ac
Akib Reza <akib.reza23@gmail.com>
2023-09-29 12:30:15 +0600

    v4.1 added animation

--- script.js
+++ script.js
@@ -1,3 +1,21 @@
 function goBack() {
   window.history.back();
 }
+
+document.addEventListener("DOMContentLoaded", function () {
+  const parentElement = document.getElementById("parent"); // Replace "parent" with the actual ID of the parent element
+
+  // Get all the child elements
+  const children = parentElement.children;
+
+  // Loop through the children and add a class based on their index (0-based)
+  for (let i = 0; i < children.length; i++) {
+    if (i % 2 === 0) {
+      // Add a class to even children
+      children[i].classList.add("animate__animated", "animate__fadeInLeft");
+    } else {
+      // Add a class to odd children
+      children[i].classList.add("animate__animated", "animate__fadeInRight");
+    }
+  }
+});
